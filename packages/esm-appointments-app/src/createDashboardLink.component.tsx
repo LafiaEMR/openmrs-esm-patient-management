964089e4ea239490b4fb95abce07bf713e38f131
alok <alok.subedi@seerglobalsolutions.com>
2024-12-29 23:26:07 +0545

    Refactor(appointmets): Change header icon and left nav bar link

--- packages/esm-appointments-app/src/createDashboardLink.component.tsx
+++ packages/esm-appointments-app/src/createDashboardLink.component.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 import { ConfigurableLink } from '@openmrs/esm-framework';
 import { BrowserRouter, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import LafiaAppointmentsIcon from '../public/lafia-appointments-Icon';
+import styles from './createDashboardLink.scss';
 
 export interface DashboardLinkConfig {
   name: string;
@@ -23,11 +25,14 @@ function DashboardExtension({ dashboardLinkConfig }: { dashboardLinkConfig: Dash
 
   return (
     <ConfigurableLink
-      className={classNames('cds--side-nav__link', {
-        'active-left-nav-link': navLink.match(name),
-      })}
+      className={`cds--side-nav__link ${navLink.match(name) ? styles.activeLeftNavLink : ''}`}
       to={`${spaBasePath}/${name}`}>
-      {t('appointments', 'Appointments')}
+      <div className={navLink.match(name) ? styles.activeIcon : styles.inactiveIcon}>
+        <LafiaAppointmentsIcon />
+      </div>
+      <span className={navLink.match(name) ? styles.activeTitle : styles.inactiveTitle}>
+        {t('appointments', 'Appointments')}
+      </span>
     </ConfigurableLink>
   );
 }
